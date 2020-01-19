14afeadfaae145f2a6eb5bbbb92c3a8692ed6efc
PamelaNeves <pamela22neves@gmail.com>
2020-01-19 15:13:43 +0100

    tudo rodando

--- cinema-microservice/movies-service/src/index.js
+++ cinema-microservice/movies-service/src/index.js
@@ -1,5 +1,5 @@
 //index.js
-require("dotenv-safe").config({example: './src/.env'});
+require("dotenv-safe").config({example: './.env'});
 const movies = require('./api/movies');
 const server = require("./server/server");
 const repository = require("./repository/repository");
